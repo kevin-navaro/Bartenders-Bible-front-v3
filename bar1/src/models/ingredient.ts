fe1d4f9bcdd1ae2178e1bcc6f67ff370f36d5982
kevin-navaro <kevin.navaro1@gmail.com>
2023-03-15 12:00:37 +0100

    backend

--- bar1/src/models/ingredient.ts
+++
@@ -1,8 +0,0 @@
-
-export interface Ingredients
-{
-  id: number;
-  name: string;
-  image: string;
-  hasAlcohol: boolean;
-}
--- bar1/src/models/recipes.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Recipes
-{
-  id: number;
-  name: string;
-  description: string;
-  image: string;
-  hasAlcohol: boolean;
-}
